abafff3923199deb0521909f8d0bc5ade25ba1f7
Pawel Tomaszek <p.tomaszek.cs@gmail.com>
2016-10-17 20:39:14 +0200

    yup, here it is

---
+++ README.md
@@ -0,0 +1,13 @@
+Just Icons for Google Chrome
+----------------------------------------
+
+**Click to hide titles of your bookmarks.
+Click again to restore them.**
+
+**PRO TIP:** for the safety of your bookmarks please backup them before the very first use of the extension.
+Why? Actually the extension removes (not simply hides!) titles from the bookmarks and stores them in Extension's memory for future restoration. It means that if you use Chrome Sync, bookmarks on your other devices will be affected. If your Chrome crashes for some reason, it's possible that you'll be left without a way to restore bookmark titles. You've been warned ;).
+
+Feel free to ask questions.
+
+Enjoy your new aesthetic bookmarks bar!
+
---
+++ background.js
@@ -0,0 +1,71 @@
+chrome.runtime.onInstalled.addListener(function (object) {
+    chrome.notifications.create('', {
+        title: 'My Dearest Aesthetic Minimalist Friend',
+        type: "basic",
+        iconUrl: "icons/icon_48.png",
+        message: 'Click the extension icon to show/hide bookmarks names! \n\n(PRO TIP: for the safety of your bookmarks please backup them before the first use).',
+        contextMessage: 'Feel free to dismiss the popup now.',
+        requireInteraction: true
+    });
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.storage.local.get({'namesOn' : true}, function(data){
+        if(data['namesOn']){
+            chrome.storage.local.clear(function(){
+                chrome.storage.local.set({'namesOn' : false}, function(){
+                    takeNamesOff();
+                });
+            });
+        } else{
+            chrome.storage.local.set({'namesOn' : true}, function(){
+                putNamesOn();
+            });
+         }
+    });
+});
+
+var takeNamesOff = function() {
+    chrome.bookmarks.getTree(function(bookmarkTree) {
+         var bookmarkBar = bookmarkTree[0]['children'][0];
+         performForAllIn(bookmarkBar, takeNameOff);
+    });
+}
+
+var putNamesOn = function() {
+    chrome.bookmarks.getTree(function(bookmarkTree) {
+         var bookmarkBar = bookmarkTree[0]['children'][0];
+         performForAllIn(bookmarkBar, putNameOn);
+    });
+}
+
+var performForAllIn = function(node, action) {
+    if (node.hasOwnProperty('children')) {
+        for (var i = 0; i < node['children'].length; i++) {
+            var folder = node['children'][i];
+            action(folder);
+            performForAllIn(folder, action);
+        }
+    } else {
+        action(node);
+    }
+}
+
+var takeNameOff = function(node) {
+    var original = {};
+    original[node['id']] = node['title'];
+
+    chrome.storage.local.set(original, function(){
+        chrome.bookmarks.update(node.id, {'title' : ''});
+    });
+}
+
+var putNameOn = function(node) {
+    chrome.storage.local.get(null, function(data){
+        var originalTitle = data[node.id];
+        chrome.bookmarks.update(node.id, {'title' : originalTitle});
+    });
+}
+
+
+//chrome.management.onDisabled.addListener(function callback) TODO revert
