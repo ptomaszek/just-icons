1670499bd54ede44b9b8dee9e0630c8524f9d771
Paweł Tomaszek <ptomaszek@users.noreply.github.com>
2021-05-16 15:55:45 +0200

    Format code; advance to arrow functions

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.runtime.onInstalled.addListener(function (object) {
+chrome.runtime.onInstalled.addListener((object) => {
     chrome.notifications.create('', {
         title: 'My Dearest Aesthetic Minimalist Friend',
         type: "basic",
@@ -9,40 +9,41 @@ chrome.runtime.onInstalled.addListener(function (object) {
     });
 });
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.storage.local.get({'namesOn' : true}, function(data){
-        if(data['namesOn']){
-            chrome.storage.local.clear(function(){
-                chrome.storage.local.set({'namesOn' : false}, function(){
+chrome.browserAction.onClicked.addListener((tab) => {
+    chrome.storage.local.get({'namesOn': true}, (data) => {
+        if (data['namesOn']) {
+            chrome.storage.local.clear(() => {
+                chrome.storage.local.set({'namesOn': false}, () => {
                     takeNamesOff();
                 });
             });
-        } else{
-            chrome.storage.local.set({'namesOn' : true}, function(){
+        } else {
+            chrome.storage.local.set({'namesOn': true}, () => {
                 putNamesOn();
             });
-         }
+        }
     });
 });
 
-var takeNamesOff = function() {
-    chrome.bookmarks.getTree(function(bookmarkTree) {
-         var bookmarkBar = bookmarkTree[0]['children'][0];
-         performForAllIn(bookmarkBar, takeNameOff);
+let takeNamesOff = () => {
+    console.debug("Taking titles off...")
+    chrome.bookmarks.getTree((bookmarkTree) => {
+        let bookmarkBar = bookmarkTree[0]['children'][0];
+        performForAllIn(bookmarkBar, takeNameOff);
     });
 }
 
-var putNamesOn = function() {
-    chrome.bookmarks.getTree(function(bookmarkTree) {
-         var bookmarkBar = bookmarkTree[0]['children'][0];
-         performForAllIn(bookmarkBar, putNameOn);
+let putNamesOn = () => {
+    chrome.bookmarks.getTree((bookmarkTree) => {
+        let bookmarkBar = bookmarkTree[0]['children'][0];
+        performForAllIn(bookmarkBar, putNameOn);
     });
 }
 
-var performForAllIn = function(node, action) {
+let performForAllIn = (node, action) => {
     if (node.hasOwnProperty('children')) {
-        for (var i = 0; i < node['children'].length; i++) {
-            var folder = node['children'][i];
+        for (let i = 0; i < node['children'].length; i++) {
+            let folder = node['children'][i];
             action(folder);
             performForAllIn(folder, action);
         }
@@ -51,18 +52,19 @@ var performForAllIn = function(node, action) {
     }
 }
 
-var takeNameOff = function(node) {
-    var original = {};
+let takeNameOff = (node) => {
+    let original = {};
     original[node['id']] = node['title'];
 
-    chrome.storage.local.set(original, function(){
-        chrome.bookmarks.update(node.id, {'title' : ''});
+    chrome.storage.local.set(original, () => {
+        chrome.bookmarks.update(node.id, {'title': ''});
     });
 }
 
-var putNameOn = function(node) {
-    chrome.storage.local.get(null, function(data){
-        var originalTitle = data[node.id];
-        chrome.bookmarks.update(node.id, {'title' : originalTitle});
+let putNameOn = (node) => {
+    console.debug("Putting titles back...")
+    chrome.storage.local.get(null, (data) => {
+        let originalTitle = data[node.id];
+        chrome.bookmarks.update(node.id, {'title': originalTitle});
     });
 }
